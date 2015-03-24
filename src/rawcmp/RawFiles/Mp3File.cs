6ed4e0f2eb07464ff0df528994fd32d1b323ac70
vurdalakov <vurdalakov@gmail.com>
2015-03-24 07:49:36 +0200

    Initial MP3 format support

---
+++ src/rawcmp/RawFiles/Mp3File.cs
@@ -0,0 +1,19 @@
+﻿namespace Vurdalakov.RawCompare
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Text;
+
+    public class Mp3File : RawFile
+    {
+        protected override void Read(BinaryReader binaryReader)
+        {
+        }
+
+        public override Boolean IsSameAs(RawFile rawFile)
+        {
+            return false;
+        }
+    }
+}

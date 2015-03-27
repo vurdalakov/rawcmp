153b9d72ea449ce1500cb3c94e31efa13e45078d
vurdalakov <vurdalakov@gmail.com>
2015-03-27 11:45:06 +0200

    ZipFile refactored

--- src/rawcmp/RawFiles/ZipFile.cs
+++ src/rawcmp/RawFiles/ZipFile.cs
@@ -28,8 +28,26 @@
     {
         private const UInt32 _signature = 0x06054b50;
 
-        protected override Boolean IsSupported(BinaryReader binaryReader)
+        private Int64 FindEndOfCentralDirectoryRecordOffset(Stream stream)
         {
+            var start = Environment.TickCount;
+
+            var length = stream.Length;
+
+            if (length < 22)
+            {
+                return -1; // ZIP file can't be smaller than 22 bytes
+            }
+
+            var size = (Int32)Math.Min(length, 66560); // max comment size is 65536
+
+            stream.Seek(-size, SeekOrigin.End);
+
+            var buffer = new Byte[size];
+            stream.Read(buffer, 0, size);
+
+            var binaryReader = new BinaryReader(new MemoryStream(buffer));
+
             try
             {
                 binaryReader.Seek(-17, SeekOrigin.End);
@@ -38,71 +56,61 @@
                     var endOfCentralDirectoryRecordOffset = binaryReader.Seek(-5, SeekOrigin.Current);
                     if (_signature == binaryReader.ReadUInt32())
                     {
-                        return true;
+                        binaryReader.Skip(16);
+
+                        var commentSize = binaryReader.ReadUInt16();
+                        return (binaryReader.GetPosition() + commentSize) == binaryReader.GetLength() ? length - size + endOfCentralDirectoryRecordOffset : -1;
                     }
                 }
-                while (binaryReader.BaseStream.Position > 0);
+                while (binaryReader.GetPosition() > 4);
             }
-            catch { }
+            catch (Exception ex)
+            {
+                Console.WriteLine("{0}", ex.Message);
+            }
 
-            return false;
+            Console.WriteLine("{0}", (double)Environment.TickCount - start);
+
+            return -1;
+        }
+
+        protected override Boolean IsSupported(BinaryReader binaryReader)
+        {
+            return FindEndOfCentralDirectoryRecordOffset(binaryReader.BaseStream) > 0;
         }
 
         public ZipFileRecord[] FileRecords { get; private set; }
 
         protected override void Read(BinaryReader binaryReader)
         {
-            if (binaryReader.GetLength() < 22)
+            var endOfCentralDirectoryRecordOffset = FindEndOfCentralDirectoryRecordOffset(binaryReader.BaseStream);
+            if (endOfCentralDirectoryRecordOffset < 0)
             {
-                throw new Exception("ZIP file can't be smaller than 22 bytes");
+                throw new Exception("Invalid file format");
             }
 
-            var records = new List<ZipFileRecord>();
+            binaryReader.Seek(endOfCentralDirectoryRecordOffset, SeekOrigin.Begin);
 
-            try
+            binaryReader.Skip(10);
+            var entries = binaryReader.ReadUInt16();
+            binaryReader.Skip(4);
+            var centralDirOffset = binaryReader.ReadUInt32();
+
+            binaryReader.Seek(centralDirOffset, SeekOrigin.Begin);
+
+            var records = new List<ZipFileRecord>(entries);
+            for (int i = 0; i < entries; i++)
             {
-                binaryReader.Seek(-17, SeekOrigin.End);
-                do
-                {
-                    var endOfCentralDirectoryRecordOffset = binaryReader.Seek(-5, SeekOrigin.Current);
-                    UInt32 signature = binaryReader.ReadUInt32();
-                    if (_signature == signature)
-                    {
-                        binaryReader.Skip(6);
-
-                        UInt16 entries = binaryReader.ReadUInt16();
-                        Int32 centralSize = binaryReader.ReadInt32();
-                        UInt32 centralDirOffset = binaryReader.ReadUInt32();
-                        UInt16 commentSize = binaryReader.ReadUInt16();
-
-                        // check if comment field is the very last data in file
-                        if (binaryReader.BaseStream.Position + commentSize != binaryReader.BaseStream.Length)
-                        {
-                            throw new Exception("Invalid archive comment");
-                        }
-
-                        binaryReader.Seek(centralDirOffset, SeekOrigin.Begin);
-
-                        for (int i = 0; i < entries; i++)
-                        {
-                            var record = ReadEntry(binaryReader);
-                            records.Add(record);
-                        }
-
-                        if (binaryReader.GetPosition() != endOfCentralDirectoryRecordOffset)
-                        {
-                            throw new Exception("Invalid central directory record");
-                        }
-
-                        FileRecords = records.ToArray();
-                        return;
-                    }
-                }
-                while (binaryReader.BaseStream.Position > 0);
+                var record = ReadEntry(binaryReader);
+                records.Add(record);
             }
-            catch { }
 
-            throw new Exception("Central directory record not found or invalid");
+            if (binaryReader.GetPosition() != endOfCentralDirectoryRecordOffset)
+            {
+                throw new Exception("Invalid central directory record");
+            }
+
+            FileRecords = records.ToArray();
         }
 
         private ZipFileRecord ReadEntry(BinaryReader binaryReader)

8d6d154d4eaa8f6b201b99c08088d630b1a35da9
vurdalakov <vurdalakov@gmail.com>
2015-03-28 13:04:31 +0200

    AttributeExtensions class added

---
+++ src/rawcmp/Vurdalakov/AttributeExtensions.cs
@@ -0,0 +1,18 @@
+﻿namespace Vurdalakov
+{
+    using System;
+    using System;
+
+    public static class AttributeExtensions
+    {
+        public static TValue GetAttributeValue<TAttribute, TValue>(this Type type, Func<TAttribute, TValue> valueSelector) where TAttribute : Attribute
+        {
+            var attribute = type.GetCustomAttributes(typeof(TAttribute), true).FirstOrDefault() as TAttribute;
+            if (attribute != null)
+            {
+                return valueSelector(attribute);
+            }
+            return default(TValue);
+        }
+    }
+}
